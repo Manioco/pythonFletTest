d6a927e89f1fe0d2477e7397f22063c16b19d0ca
Manioco <prausartudo@gmail.com>
2023-09-26 12:04:33 -0300

    Melhorias da implementação do banco de dados

--- todoto.py
+++ todoto.py
@@ -1,5 +1,19 @@
 import flet as ft
 import sqlite3
+import time
+import glob
+import os
+
+
+"""
+Fazer uma verificação para garantir que nenhuma tarefa seja repetida
+Criar um código para identificar cada tarefa
+adicionar as colunas de código nos databases
+criar um database de LOG
+inserir um banco de dados para os ativos
+inserir um banco de dados para os deletados
+"""
+
 
 
 # Class to create tasks
@@ -54,6 +68,7 @@ class Task(ft.UserControl):
                 ),
             ],
         )
+
         return ft.Column(controls=[self.display_view, self.edit_view])
 
     async def edit_clicked(self, e):
@@ -165,6 +180,29 @@ class TodoApp(ft.UserControl):
         await super().update_async()
 
 
+def db_execute(date:str, query, params = []):
+    with sqlite3.connect(f'database{date}.db') as con:
+        cur = con.cursor()
+        cur.execute(query, params)
+        con.commit()
+        print(f"Executed query: {query}")
+        return cur.fetchall()
+
+def delete_old_databases(current_date: str):
+    # Obtém uma lista de todos os bancos de dados no diretório atual
+    database_files = glob.glob('*.db')
+
+    for db_file in database_files:
+        # Extrai a data do nome do arquivo (assumindo que o formato seja "tasksDD-MM-YYYY.db")
+        filename = os.path.basename(db_file)
+        date_part = filename.replace('tasks', '').replace('.db', '')
+
+        # Compara a data do arquivo com a data atual
+        if date_part != current_date:
+            # Se a data do arquivo for diferente da data atual, exclua o arquivo
+            os.remove(db_file)
+            print(f"Deleted old database: {db_file}")
+
 # async function to create the page
 async def main(page: ft.Page):
     page.title = "ToDo App"
@@ -173,7 +211,11 @@ async def main(page: ft.Page):
     await page.update_async()
 
     # create application instance
-    app = TodoApp()
+    # date = time.strftime("%d%m%Y")
+    date = time.strftime("%m")
+    delete_old_databases(date)
+    db_execute(date, f'CREATE TABLE IF NOT EXISTS tasks{date} (name, status)')
+    app = TodoApp(date)
 
     # add application's root control to the page
     await page.add_async(app)
